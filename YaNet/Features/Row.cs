e5b5fe8f7cddedc7b06c2509810b75fba91bc3ba
GoncharovMain <yuriy.goncharov@royalpay.online>
2022-12-13 17:22:35 +0300

    Define type from child type into type Row.

--- YaNet/Features/Row.cs
+++ YaNet/Features/Row.cs
@@ -6,42 +6,61 @@ namespace YaNet.Features
 	public class Row
 	{
 		private Mark _row;
-		private int _position;
 
-		public Row(Row row) : this(row._position, row._row) { }
+		public Type Type { get; private set; }
 
-		public Row(int position, Mark row) => (_position, _row) = (position, row);
+
+		protected Row(Row row, Type type) : this(row._row, type) { }
+
+		protected Row(Mark row, Type type) => (_row, Type) = (row, type);
+
+
+		public Row(Row row) : this(row._row) => Type = typeof(Row);
+
+		public Row(Mark row) => (_row, Type) = (row, typeof(Row));
 	}
 
 	public class KeyRow : Row
 	{
 		private Mark _key;
 
-		public KeyRow(Row row, Mark key) : base(row) => _key = key;
 
-		public KeyRow(int position, Mark row, Mark key) 
-			: base(position, row) => _key = key;
+		protected KeyRow(Row row, Mark key, Type type) 
+			: base(row, type) => _key = key;
+
+		protected KeyRow(Mark row, Mark key, Type type) 
+			: base(row, type) => _key = key;
+
+
+		public KeyRow(Row row, Mark key) 
+			: base(row, typeof(KeyRow)) => _key = key;
+
+		public KeyRow(Mark row, Mark key) 
+			: base(row, typeof(KeyRow)) => _key = key;
 	}
 
 	public class KeyValueRow : KeyRow
 	{
 		private Mark _value;
 
-		public KeyValueRow(Row row, Mark key, Mark value) 
-			: base(row, key) => _value = value;
 
-		public KeyValueRow(int position, Mark row, Mark key, Mark value) 
-			: base(position, row, key) => _value = value;
+		public KeyValueRow(Row row, Mark key, Mark value) 
+			: base(row, key, typeof(KeyValueRow)) => _value = value;
+
+		public KeyValueRow(Mark row, Mark key, Mark value) 
+			: base(row, key, typeof(KeyValueRow)) => _value = value;
 	}
 
 	public class ItemRow : Row
 	{
 		private Mark _item;
+		
 
-		public ItemRow(Row row, Mark item) : base(row) => _item = item;
+		public ItemRow(Row row, Mark item) 
+			: base(row, typeof(ItemRow)) => _item = item;
 
-		public ItemRow(int position, Mark row, Mark item) 
-			: base(position, row) => _item = item;
+		public ItemRow(Mark row, Mark item) 
+			: base(row, typeof(ItemRow)) => _item = item;
 	}
 
 	public class ItemKeyValueRow : Row
@@ -51,14 +70,15 @@ namespace YaNet.Features
 		private Mark _value;
 
 		public ItemKeyValueRow(Row row, Mark item, Mark key, Mark value)
-			: base(row)
+			: base(row, typeof(ItemKeyValueRow))
 		{
 			_item = item;
 			_key = key;
 			_value = value;
 		}
 
-		public ItemKeyValueRow(int position, Mark row, Mark item, Mark key, Mark value) : base(position, row)
+		public ItemKeyValueRow(Mark row, Mark item, Mark key, Mark value) 
+			: base(row, typeof(ItemKeyValueRow))
 		{
 			_item = item;
 			_key = key;
